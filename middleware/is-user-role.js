f272154c4ffb99ffaff5a0a7683e70dfbbeb6a9b
Uziel <uzielmelitonh@gmail.com>
2021-04-03 20:20:01 -0600

    Editing rules

--- middleware/is-user-role.js
+++ middleware/is-user-role.js
@@ -1,6 +1,10 @@
 export default  async (context) => {
     const { redirect, $auth } = context;
-    const token = await $auth.strategy.token.get()    
+    const token = await $auth.strategy.token.get();
+    if(!token){
+        $auth.logout();
+        return redirect('/unirse/login')
+    } 
     const b64Encoded = token.split(' ')[1].split('.')[1];    ;
     const data = JSON.parse( Buffer.from(b64Encoded, 'base64').toString());    
     
