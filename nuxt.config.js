4d61788c90ac8c3f341a0ba9ba520d5e2ce51f46
Uziel Hernandez <uzielmelitonh@gmail.com>
2021-10-16 18:25:43 -0500

    add splah screen

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,23 +9,49 @@ export default {
     // title: 'ReadAndRead',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width:device-width, initial-scale:1' },
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
+      { rel: "apple-touch-startup-image", href: "/splash/apple-splash-2048-2732.jpeg", media: "(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" },
+      { rel: "apple-touch-startup-image", href: "/splash/apple-splash-2732-2048.jpeg", media: "(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" },
+      { rel: "apple-touch-startup-image", href: "/splash/apple-splash-1668-2388.jpeg", media: "(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" },
+      { rel: "apple-touch-startup-image", href: "/splash/apple-splash-2388-1668.jpeg", media: "(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" },
+      { rel: "apple-touch-startup-image", href: "/splash/apple-splash-1536-2048.jpeg", media: "(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" },
+      { rel: "apple-touch-startup-image", href: "/splash/apple-splash-2048-1536.jpeg", media: "(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" },
+      { rel: "apple-touch-startup-image", href: "/splash/apple-splash-1668-2224.jpeg", media: "(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" },
+      { rel: "apple-touch-startup-image", href: "/splash/apple-splash-2224-1668.jpeg", media: "(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" },
+      { rel: "apple-touch-startup-image", href: "/splash/apple-splash-1620-2160.jpeg", media: "(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" },
+      { rel: "apple-touch-startup-image", href: "/splash/apple-splash-2160-1620.jpeg", media: "(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" },
+      { rel: "apple-touch-startup-image", href: "/splash/apple-splash-1284-2778.jpeg", media: "(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" },
+      { rel: "apple-touch-startup-image", href: "/splash/apple-splash-2778-1284.jpeg", media: "(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)" },
+      { rel: "apple-touch-startup-image", href: "/splash/apple-splash-1170-2532.jpeg", media: "(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" },
+      { rel: "apple-touch-startup-image", href: "/splash/apple-splash-2532-1170.jpeg", media: "(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)" },
+      { rel: "apple-touch-startup-image", href: "/splash/apple-splash-1125-2436.jpeg", media: "(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" },
+      { rel: "apple-touch-startup-image", href: "/splash/apple-splash-2436-1125.jpeg", media: "(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)" },
+      { rel: "apple-touch-startup-image", href: "/splash/apple-splash-1242-2688.jpeg", media: "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" },
+      { rel: "apple-touch-startup-image", href: "/splash/apple-splash-2688-1242.jpeg", media: "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)" },
+      { rel: "apple-touch-startup-image", href: "/splash/apple-splash-828-1792.jpeg", media: "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" },
+      { rel: "apple-touch-startup-image", href: "/splash/apple-splash-1792-828.jpeg", media: "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" },
+      { rel: "apple-touch-startup-image", href: "/splash/apple-splash-1242-2208.jpeg", media: "(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" },
+      { rel: "apple-touch-startup-image", href: "/splash/apple-splash-2208-1242.jpeg", media: "(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)" },
+      { rel: "apple-touch-startup-image", href: "/splash/apple-splash-750-1334.jpeg", media: "(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" },
+      { rel: "apple-touch-startup-image", href: "/splash/apple-splash-1334-750.jpeg", media: "(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" },
+      { rel: "apple-touch-startup-image", href: "/splash/apple-splash-640-1136.jpeg", media: "(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" },
+      { rel: "apple-touch-startup-image", href: "/splash/apple-splash-1136-640.jpeg", media: "(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" },
     ],
-    script: [         
+    script: [
       {
         src: 'https://apis.google.com/js/api:client.js',
-        defer: true,        
+        defer: true,
       },
       {
-        src: "https://www.google.com/recaptcha/api.js?render=6LdQSkkaAAAAANrCpXGxHSzUEN3khnp3A4_pqn4T",
-        defer:true
+        src: "https://www.google.com/recaptcha/api.js?render:6LdQSkkaAAAAANrCpXGxHSzUEN3khnp3A4_pqn4T",
+        defer: true
 
       }
-    ] 
+    ],
 
   },
   loading: {
@@ -49,21 +75,22 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',    
+    '@nuxtjs/vuetify',
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    '@nuxtjs/auth-next',    
+    '@nuxtjs/auth-next',
     '@nuxtjs/pwa',
   ],
 
   pwa: {
-    meta:{
+    meta: {
       title: 'Read&Read',
-      author: 'Team Read&Read'
+      author: 'Team Read&Read',
+      mobileAppIOS: true,
     },
     icon: {
       fileName: 'icon.png',
@@ -71,11 +98,11 @@ export default {
       purpose: 'any'
     },
     manifest: {
-      name: 'Read&Read App',
+      name: 'Read&Read',
       short_name: 'Read&Read',
       description: 'Aplicación de venta de libros digitales, disfruta leerlo desde nuestra plataforma',
       charset: 'utf-8',
-      lang: 'es',  
+      lang: 'es',
       useWebmanifestExtension: false,
       background_color: '#062247',
       start_url: '/'
@@ -84,20 +111,20 @@ export default {
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {
-    baseURL: process.env.BASE_URL || 'http://localhost:8080/api/v1/',  
+    baseURL: process.env.BASE_URL || 'http://localhost:8080/api/v1/',
   },
 
   // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: false,      
+      dark: false,
       themes: {
         dark: {
           primary: '#0078D4',
           secondary: '#48D77A',
-          accent: '#2F80ED',                       
-                  
+          accent: '#2F80ED',
+
           error: '#FF5252',
           info: '#2196F3',
           success: '#48D77A',
@@ -109,7 +136,7 @@ export default {
           cards: '#1E1E1E',
           footer: '#121C25',
           footer2: '#121212',
-          navbar: '#292A2D',          
+          navbar: '#292A2D',
           icons: '#E8EAED',
           search: '#202123',
           title: '#ffffff',
@@ -119,13 +146,13 @@ export default {
         light: {
           primary: '#062146',
           secondary: '#48D77A',
-          accent: '#2F80ED',                       
+          accent: '#2F80ED',
 
           error: '#FF5252',
           info: '#2196F3',
           success: '#48D77A',
           warning: '#FFC107',
-          
+
           section: '#062146',
           section2: '#2F80ED',
           background: '#FFFFFF',
@@ -137,13 +164,13 @@ export default {
           title: '#555555',
 
           backgrounduser: '#FBFAFF'
-          
+
         },
       }
     }
   },
 
-  auth: {    
+  auth: {
     strategies: {
       local: {
         endpoints: {
@@ -151,12 +178,12 @@ export default {
           // user: { url: 'me', method: 'get', propertyName: 'data' },
           user: false,
           logout: false
-        },        
-        tokenType: false        
+        },
+        tokenType: false
       }
-    
-    },    
-    redirect:{
+
+    },
+    redirect: {
       login: false,
       home: false,
       callback: false,
@@ -167,8 +194,8 @@ export default {
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
     vendor: ['vue-pdf'],
-   extend(config, ctx) {
+    extend(config, ctx) {
       config.output.globalObject = 'this'
-   }
+    }
   }
 }
